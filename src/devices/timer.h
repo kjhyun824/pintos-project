9ccaade5faa05ffd9ca72c76f2ced10253522052
Heena Kwag <dotaitch@gmail.com>
2016-10-10 02:56:47 +0900

    75.2%

---
+++ src/devices/timer.h
@@ -0,0 +1,50 @@
+#ifndef DEVICES_TIMER_H
+#define DEVICES_TIMER_H
+
+#include <round.h>
+#include <stdint.h>
+
+// KJH
+#include "threads/synch.h"
+#include "threads/thread.h"
+
+/* Number of timer interrupts per second. */
+#define TIMER_FREQ 100
+
+void timer_init (void);
+void timer_calibrate (void);
+
+int64_t timer_ticks (void);
+int64_t timer_elapsed (int64_t);
+
+/* Sleep and yield the CPU to other threads. */
+void timer_sleep (int64_t ticks);
+void timer_msleep (int64_t milliseconds);
+void timer_usleep (int64_t microseconds);
+void timer_nsleep (int64_t nanoseconds);
+
+// KJH
+/*
+void sleep_insert(int64_t wakeup_ticks, struct thread *t);
+void timer_unblock();
+*/
+static bool wakeup_time_less(const struct list_elem *a_,const struct list_elem *b_, void *aux UNUSED);
+void timer_unblock();
+
+/* Busy waits. */
+void timer_mdelay (int64_t milliseconds);
+void timer_udelay (int64_t microseconds);
+void timer_ndelay (int64_t nanoseconds);
+
+void timer_print_stats (void);
+
+// KJH
+/*
+struct sleep_list {
+	struct thread *blocked_thread;
+	int64_t wakeup_time;
+	struct sleep_list *next;
+};
+*/
+
+#endif /* devices/timer.h */
